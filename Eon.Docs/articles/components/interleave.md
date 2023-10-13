bc58b252b3bcb0b46cd0d45fdb435de0f81ccf75
bmazzarol <bmazzarol@gmail.com>
2023-10-13 21:52:21 +0800

    feat: interleave docs and tests [ci]

---
+++ Eon.Docs/articles/components/interleave.md
@@ -0,0 +1,7 @@
+﻿# Interleave
+
+@Eon.Schedule.Interleave* emits a @Eon.Duration from the `second`
+@Eon.Schedule between each emission of the `first` @Eon.Schedule until either
+@Eon.Schedule completes
+
+[!code-csharp[Example1](../../../Eon.Tests/Examples/InterleaveTests.cs#Example1)]
---
+++ Eon.Tests/Examples/InterleaveTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Eon.Tests.Examples;
+
+public static class InterleaveTests
+{
+    [Fact(DisplayName = "Interleave returns the durations from both schedules interleaved")]
+    public static void Case1()
+    {
+        #region Example1
+        Schedule interleave = Schedule
+            .Linear(TimeSpan.FromSeconds(1))
+            .Take(3)
+            .Interleave(Schedule.Forever);
+
+        using var enumerator = interleave.GetEnumerator();
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(1));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(0));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(2));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(0));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(3));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(0));
+        enumerator.MoveNext().Should().BeFalse();
+
+        #endregion
+    }
+}

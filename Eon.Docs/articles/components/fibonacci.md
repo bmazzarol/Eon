9667682bb59a582d130c0c5b41bcb98874b1ca7b
bmazzarol <bmazzarol@gmail.com>
2023-10-14 10:09:11 +0800

    feat: fibonacci docs and tests [ci]

---
+++ Eon.Docs/articles/components/fibonacci.md
@@ -0,0 +1,8 @@
+﻿# Fibonacci
+
+@Eon.Schedule.Fibonacci* returns an infinite stream of `space` @Eon.Duration
+increasing based on
+a [fibonacci sequence](https://en.wikipedia.org/wiki/Fibonacci_sequence) from a
+starting `seed` @Eon.Duration
+
+[!code-csharp[Example1](../../../Eon.Tests/Examples/FibonacciTests.cs#Example1)]
---
+++ Eon.Tests/Examples/FibonacciTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Eon.Tests.Examples;
+
+public static class FibonacciTests
+{
+    [Fact(
+        DisplayName = "Fibonacci returns a stream of the duration based on a fibonacci sequence from a starting seed"
+    )]
+    public static void Case1()
+    {
+        #region Example1
+        Schedule fibonacci = Schedule.Fibonacci(TimeSpan.FromSeconds(1)).Take(5);
+
+        using var enumerator = fibonacci.GetEnumerator();
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(1));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(1));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(2));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(3));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(5));
+        enumerator.MoveNext().Should().BeFalse();
+
+        #endregion
+    }
+}

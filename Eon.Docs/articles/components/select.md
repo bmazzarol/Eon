59339802ac3142bbd25520c6af8938305cd618a5
bmazzarol <bmazzarol@gmail.com>
2023-10-14 09:43:35 +0800

    feat: select docs and tests [ci]

---
+++ Eon.Docs/articles/components/select.md
@@ -0,0 +1,10 @@
+﻿# Select
+
+@Eon.Schedule.Select* transforms each emissions from a @Eon.Schedule based on a
+provided `map` function
+
+[!code-csharp[Example1](../../../Eon.Tests/Examples/SelectTests.cs#Example1)]
+
+With current `index`,
+
+[!code-csharp[Example2](../../../Eon.Tests/Examples/SelectTests.cs#Example2)]
---
+++ Eon.Tests/Examples/SelectTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Eon.Tests.Examples;
+
+public static class SelectTests
+{
+    [Fact(DisplayName = "Select transforms durations in a schedule")]
+    public static void Case1()
+    {
+        #region Example1
+        Schedule interleave = Schedule.Linear(TimeSpan.FromSeconds(1)).Take(4).Select(x => x + x);
+
+        using var enumerator = interleave.GetEnumerator();
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(2));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(4));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(6));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(8));
+        enumerator.MoveNext().Should().BeFalse();
+
+        #endregion
+    }
+
+    [Fact(DisplayName = "Select with index transforms durations in a schedule")]
+    public static void Case2()
+    {
+        #region Example2
+        Schedule interleave = Schedule
+            .Linear(TimeSpan.FromSeconds(1))
+            .Take(4)
+            .Select((x, i) => x + (Duration)TimeSpan.FromSeconds(i));
+
+        using var enumerator = interleave.GetEnumerator();
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(1));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(3));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(5));
+        enumerator.MoveNext().Should().BeTrue();
+        enumerator.Current.Should().Be(TimeSpan.FromSeconds(7));
+        enumerator.MoveNext().Should().BeFalse();
+
+        #endregion
+    }
+}

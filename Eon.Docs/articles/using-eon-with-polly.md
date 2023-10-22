fd03f5583ade04f088f4e256e4d4b0dbf440fe8f
bmazzarol <bmazzarol@gmail.com>
2023-10-22 23:21:28 +0800

    docs: examples using polly with eon

---
+++ Eon.Docs/articles/using-eon-with-polly.md
@@ -0,0 +1,16 @@
+﻿# How to use with Polly.Net
+
+[Polly](https://github.com/App-vNext/Polly) is a popular resilience
+and transient-fault-handling library for .NET.
+
+It exposes
+a [RetryStrategyOptions](https://github.com/App-vNext/Polly/blob/main/src/Polly.Core/Retry/RetryStrategyOptions.TResult.cs)
+which can be used to configure the retry policy.
+
+Integrating a @Eon.Schedule can be done like so,
+
+[!code-csharp[Example1](../../Eon.Tests/Examples/PollyExamples.cs#Example1)]
+
+Using an infinite @Eon.Schedule is also possible,
+
+[!code-csharp[Example1](../../Eon.Tests/Examples/PollyExamples.cs#Example2)]
---
+++ Eon.Tests/Examples/PollyExamples.cs
@@ -0,0 +1,76 @@
+﻿using Polly;
+using Polly.Retry;
+
+namespace Eon.Tests.Examples;
+
+public static class PollyExamples
+{
+    [Fact(DisplayName = "Schedule can be used with polly")]
+    public static async Task Case1()
+    {
+        #region Example1
+
+        // build any schedule using Eon
+        Schedule schedule = Schedule.Linear(1).Take(5);
+        // get the durations
+        var durations = schedule.ToArray();
+        // now create the options
+        RetryStrategyOptions options = new RetryStrategyOptions
+        {
+            MaxRetryAttempts = schedule.Count ?? int.MaxValue,
+            Delay = TimeSpan.Zero,
+            DelayGenerator = x =>
+                ValueTask.FromResult<TimeSpan?>((TimeSpan)durations[x.AttemptNumber])
+        };
+        ResiliencePipeline pipeline = new ResiliencePipelineBuilder().AddRetry(options).Build();
+        int attempts = 0;
+        await pipeline.ExecuteAsync(async token =>
+        {
+            await Task.Yield();
+            attempts++;
+            if (attempts < 5)
+            {
+                throw new Exception();
+            }
+        });
+        attempts.Should().Be(5);
+
+        #endregion
+    }
+
+    [Fact(DisplayName = "Infinite Schedule can be used with polly")]
+    public static async Task Case2()
+    {
+        #region Example2
+
+        // build any schedule using Eon
+        Schedule schedule = Schedule.Linear(1);
+        // now create the options
+        RetryStrategyOptions options = new RetryStrategyOptions
+        {
+            MaxRetryAttempts = int.MaxValue,
+            Delay = TimeSpan.Zero,
+            DelayGenerator = x =>
+            {
+                var enumerator = x.AttemptNumber == 0 ? schedule.GetEnumerator() : null;
+                return ValueTask.FromResult<TimeSpan?>(
+                    enumerator?.MoveNext() == true ? (TimeSpan)enumerator.Current : null
+                );
+            }
+        };
+        ResiliencePipeline pipeline = new ResiliencePipelineBuilder().AddRetry(options).Build();
+        int attempts = 0;
+        await pipeline.ExecuteAsync(async token =>
+        {
+            await Task.Yield();
+            attempts++;
+            if (attempts < 10)
+            {
+                throw new Exception();
+            }
+        });
+        attempts.Should().Be(10);
+
+        #endregion
+    }
+}

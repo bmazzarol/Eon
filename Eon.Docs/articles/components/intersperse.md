0adb3e2c3fe07487120e21ab3d5fbf02a044aaf5
bmazzarol <bmazzarol@gmail.com>
2023-10-22 22:24:04 +0800

    feat: intersperse docs and tests [ci]

---
+++ Eon.Docs/articles/components/intersperse.md
@@ -0,0 +1,6 @@
+﻿# Intersperse
+
+@Eon.Schedule.Intersperse* emits all @Eon.Duration from `schedule` between each
+emission in the current @Eon.Schedule
+
+[!code-csharp[Example1](../../../Eon.Tests/Examples/IntersperseTests.cs#Example1)]
---
+++ Eon/Schedule.Intersperse.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Eon;
+
+public abstract partial record Schedule
+{
+    /// <summary>
+    /// A <see cref="ScheduleTransformer"/> that intersperses the provided
+    /// <see cref="Schedule"/>`s emissions between each emission in the <see cref="Schedule"/>
+    /// </summary>
+    /// <param name="schedule"><see cref="Schedule"/> to intersperse</param>
+    [Pure]
+    public static ScheduleTransformer Intersperse(Schedule schedule) =>
+        Transform(s => s.SelectMany(schedule.Prepend));
+}
